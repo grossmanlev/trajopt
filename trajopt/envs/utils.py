92e5e9b7319bd2096cc92b24c4f72f402a9db84e
grossmanlev <lgrossman@college.harvard.edu>
2020-01-06 17:21:46 -0500

    test_mppi_critic script

--- trajopt/envs/utils.py
+++ trajopt/envs/utils.py
@@ -1,14 +1,11 @@
-from trajopt.envs.reacher import Reacher2DOFEnv
 from trajopt.envs.reacher_env import Reacher7DOFEnv
 from trajopt.envs.continual_reacher_env import ContinualReacher7DOFEnv
 from trajopt.envs.hopper import HopperEnv
 
 
-def get_environment(env_name, sparse_reward=False):
+def get_environment(env_name, sparse_reward=True):
     if env_name == 'reacher_7dof':
         return Reacher7DOFEnv(sparse_reward=sparse_reward)
-    elif env_name == 'reacher_2dof':
-        return Reacher2DOFEnv(sparse_reward=sparse_reward)
     elif env_name == 'continual_reacher_7dof':
         return ContinualReacher7DOFEnv()
     elif env_name == 'hopper':
--- trajopt/sandbox/examples/test_mppi_critic.py
+++ trajopt/sandbox/examples/test_mppi_critic.py
@@ -22,8 +22,10 @@ H = 16
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('critic', default=None,
+    parser.add_argument('--critic', default=None,
                         help='path to critic model (.pt) file')
+    parser.add_argument('--iters', default=10, type=int,
+                        help='number of random initializations to test')
     args = parser.parse_args()
 
     e = get_environment(ENV_NAME, sparse_reward=True)
@@ -33,93 +35,70 @@ if __name__ == '__main__':
     sigma = 1.0*np.ones(e.action_dim)
     filter_coefs = [sigma, 0.25, 0.8, 0.0]
 
-    critic = Critic()
-    critic.load_state_dict(torch.load(args.critic))
-    critic.eval()
-    critic.float()
+    critic = None
+    if args.critic is not None:
+        critic = Critic()
+        critic.load_state_dict(torch.load(args.critic))
+        critic.eval()
+        critic.float()
 
-    joint_limits = [[-2.2854, 1.714602],
-                    [-0.5236, 1.3963],
-                    [-1.5, 1.7],
-                    [-2.3213, 0],
-                    [-1.5, 1.5],
-                    [-1.094, 0],
-                    [-1.5, 1.5]]
-    import pdb; pdb.set_trace()
+    joint_limits = np.array([[-2.2854, 1.714602],
+                             [-0.5236, 1.3963],
+                             [-1.5, 1.7],
+                             [-2.3213, 0],
+                             [-1.5, 1.5],
+                             [-1.094, 0],
+                             [-1.5, 1.5]])
+    joint_limits /= 4.0
+
+    total_reward = 0
+    for _ in range(args.iters):
+        random_qpos = np.random.uniform(joint_limits[:, 0], joint_limits[:, 1])
+        e.set_state(random_qpos, e.init_qvel)
+
+        agent = MPPI(e, H=H, paths_per_cpu=40, num_cpu=1,
+                     kappa=25.0, gamma=1.0, mean=mean,
+                     filter_coefs=filter_coefs,
+                     default_act='mean', seed=SEED,
+                     init_seq=None)
+
+        ts = timer.time()
+        for t in tqdm(range(H_total)):
+
+            # Actor step
+            agent.train_step(critic=critic, niter=N_ITER)
+
+        total_reward += np.sum(agent.sol_reward)
+        print("Trajectory reward = %f" % np.sum(agent.sol_reward))
+
+    print("Average reward = {}".format(total_reward / float(args.iters)))
+
+    # agent = pickle.load(open('116_agent.pickle', 'rb'))
+
+    # sol_actions = np.array(agent.sol_act)  # should be (100, 7)
+    # init_seq = sol_actions[:H]
 
     # agent = MPPI(e, H=H, paths_per_cpu=40, num_cpu=1,
     #              kappa=25.0, gamma=1.0, mean=mean,
     #              filter_coefs=filter_coefs,
-    #              default_act='mean', seed=SEED)
+    #              default_act='mean', seed=SEED,
+    #              init_seq=None)
 
     # ts = timer.time()
     # for t in tqdm(range(H_total)):
 
     #     # Actor step
     #     # print(sol_actions[t:t+H][0])
-    #     agent.train_step(critic=None, niter=N_ITER)
+    #     if t < 0:
+    #         agent.train_step(critic=None, niter=N_ITER, act_sequence=sol_actions[t:t+H])
+    #     else:
+    #         agent.train_step(critic=None, niter=N_ITER)
 
-    # pickle.dump(agent, open('116_agent.pickle', 'wb'))
+    # print("Time for trajectory optimization = %f seconds" % (timer.time()-ts))
+    # # pickle.dump(agent, open('good_agent.pickle', 'wb'))
+    # print("Trajectory reward = %f" % np.sum(agent.sol_reward))
 
-    # import pdb; pdb.set_trace()
-
-
-
-
-    agent = pickle.load(open('116_agent.pickle', 'rb'))
-
-    # agent_test = pickle.load(open('test_agent.pickle', 'rb'))
-
-    # import pdb; pdb.set_trace()
-
-    # e.reset_model()
-    # for i in range(len(agent_test.sol_act)):
-    #     e.render()
-    #     e.step(agent_test.sol_act[i])
-
-    # import pdb; pdb.set_trace()
-
-    sol_actions = np.array(agent.sol_act)  # should be (100, 7)
-    init_seq = sol_actions[:H]
-
-    agent = MPPI(e, H=H, paths_per_cpu=40, num_cpu=1,
-                 kappa=25.0, gamma=1.0, mean=mean,
-                 filter_coefs=filter_coefs,
-                 default_act='mean', seed=SEED,
-                 init_seq=None)
-
-    ts = timer.time()
-    for t in tqdm(range(H_total)):
-
-        # Actor step
-        # print(sol_actions[t:t+H][0])
-        if t < 0:
-            agent.train_step(critic=None, niter=N_ITER, act_sequence=sol_actions[t:t+H])
-        else:
-            agent.train_step(critic=None, niter=N_ITER)
-
-    print("Time for trajectory optimization = %f seconds" % (timer.time()-ts))
-    # pickle.dump(agent, open('good_agent.pickle', 'wb'))
-    print("Trajectory reward = %f" % np.sum(agent.sol_reward))
-
-    # import pdb; pdb.set_trace()
-
-    # wait for user prompt before visualizing optimized trajectories
-    _ = input("Press enter to display optimized trajectory (will be played 100 times) : ")
-    for _ in range(100):
-        agent.animate_result()
-
-    # new_q = np.zeros(7)
-    # # new_q = np.array([10.99164932,  0.06841799, -1.50792112, -1.56400837, -1.52414601,
-    # #                                    0.01832143, -1.52851301])
-    # new_q[0] = 1
-
-    # e.set_state(new_q, e.init_qvel)
-    # print(e._get_obs())
-
-    # for _ in range(10):
-    #     e.render()
-    #     print(e._get_obs())
-    #     a = np.zeros(7)
-    #     e.step(a)
-    # e.close()
+    # # wait for user prompt before visualizing optimized trajectories
+    # _ = input("Press enter to display optimized trajectory (will be played 100 times) : ")
+    # for _ in range(100):
+    #     agent.animate_result()
